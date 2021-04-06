269a2b75dc061f3c05bee70ecf20a1c3ebaf42d8
ecko7700 <62030900+ecko7700@users.noreply.github.com>
2021-04-06 16:52:38 +0200

    Part 1 and 2 of Day 2 completed
    Correct Solution

--- Day2_2020.py
+++ Day2_2020.py
@@ -25,3 +25,70 @@ The first and third passwords are valid: they contain one a or nine c, both with
 
 How many passwords are valid according to their policies?
 """
+filename = 'Day2_2020_data.txt'
+f = open(filename, 'r')
+data_list = []
+
+for line in f:
+    data_list.append(line.split()) 
+#########################__________PART 1__________############################
+def test_password(data_list):
+    copy_data = data_list.copy()
+    valid_pass_cnt = 0
+    
+    for l1 in copy_data:
+        test_range_low = int(l1[0].split('-')[0])
+        test_range_hgh = int(l1[0].split('-')[1])
+        test_char = l1[1].split(':')[0]
+        test_string = l1[2]
+        test_char_count = test_string.count(test_char)
+        if test_char_count >= test_range_low and test_char_count <= test_range_hgh:
+            valid_pass_cnt +=1
+        
+        
+    return valid_pass_cnt
+
+#########################__________PART 2__________############################
+"""
+Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. 
+(Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter.
+ Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+
+Given the same example list from above:
+
+1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+"""
+def test_password_new_policy(data_list):
+    copy_data = data_list.copy()
+    valid_pass_cnt = 0
+    invalid_pass_cnt = 0
+    
+    for l1 in copy_data:
+        test_range_low = int(l1[0].split('-')[0])
+        test_pos_low = test_range_low - 1
+        test_range_hgh = int(l1[0].split('-')[1])
+        test_pos_hgh = test_range_hgh - 1
+        
+        test_char = l1[1].split(':')[0]
+        test_string = l1[2]
+        print(test_string)
+        try:
+            if test_string[test_pos_low] == test_char and test_string[test_pos_hgh] != test_char:
+                valid_pass_cnt +=1
+            elif test_string[test_pos_hgh] == test_char and test_string[test_pos_low] != test_char:
+                valid_pass_cnt +=1
+            else:
+                print("Invalid Password")
+                invalid_pass_cnt +=1
+        except IndexError:
+            invalid_pass_cnt +=1
+            print("Invalid Password")
+    return valid_pass_cnt
+
+
+
+if __name__ == '__main__':
+    print("Part 1 result: " + str(test_password(data_list)))
+    print("Part 2 result: " + str(test_password_new_policy(data_list)))
