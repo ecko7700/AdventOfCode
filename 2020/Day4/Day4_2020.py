0a367b244a427492d3b92133676f2ed26cb85eba
ecko7700 <62030900+ecko7700@users.noreply.github.com>
2021-04-11 15:38:32 +0200

    Update Day4_2020.py
    Working solution for part 2

--- 2020/Day4/Day4_2020.py
+++ 2020/Day4/Day4_2020.py
@@ -88,7 +88,86 @@ def load_test_data():
             pass_port_count +=1
     dict_passports[pass_port_count] = passports
     return dict_passports
+
+def checkbyr(byr_string):
+    if len(byr_string) !=4:
+        return False
+    try:
+        if int(byr_string)>= 1920 and int(byr_string) <= 2002:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
     
+def checkiyr(iyr_string):
+    if len(iyr_string) !=4:
+        return False
+    try:
+        if int(iyr_string)>= 2010 and int(iyr_string) <= 2020:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False      
+    
+def checkeyr(eyr_string):
+    if len(eyr_string) !=4:
+        return False
+    try:
+        if int(eyr_string)>= 2020 and int(eyr_string) <= 2030:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False   
+    
+def checkhgt(hgt_string):
+    try:
+        if hgt_string[-2:] =='cm':
+            if int(hgt_string[:-2]) >= 150 and int(hgt_string[:-2]) <= 193:
+                return True
+            else:
+                return False
+        elif hgt_string[-2:] =='in':
+             if int(hgt_string[:-2]) >= 59 and int(hgt_string[:-2]) <= 76:
+                return True
+             else:
+                return False
+        else:
+            return False
+    except ValueError:
+        return False      
+
+def checkhcl(hcl_string):
+    #hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+    valid_char = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+    if hcl_string[0] != '#':
+        return False
+    elif len(hcl_string[1:]) != 6:
+        return False
+    else:
+        copy_string = hcl_string[1:]
+        for char in copy_string:
+            if char not in valid_char:
+                return False
+        return True
+
+def checkecl(ecl_string):
+    # (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+    valid_ecl = ['amb','blu','brn','gry','grn','hzl','oth']
+    if ecl_string in valid_ecl:
+        return True
+    else:
+        return False
+
+def checkpid(pid_string):
+    # pid (Passport ID) - a nine-digit number, including leading zeroes.   
+    if len(pid_string) != 9:
+        return False
+    else:
+        return True
+ 
 def checkPassports(dict_passports):
     print("Number of passports to test: "+ str(len(dict_passports.keys())))
     valid_count = 0
@@ -99,24 +178,48 @@ def checkPassports(dict_passports):
             for fields in line:
                 check_attribute = fields[0:3]
                 if check_attribute in check_dict:
-                    check_dict[check_attribute] = True
+                    if check_attribute == 'byr':
+                        check_dict[check_attribute] = checkbyr(fields[4:])
+                    elif check_attribute == 'iyr':
+                        check_dict[check_attribute] = checkiyr(fields[4:])
+                    elif check_attribute == 'eyr':
+                        check_dict[check_attribute] = checkeyr(fields[4:])
+                    elif check_attribute == 'hgt':
+                        check_dict[check_attribute] = checkhgt(fields[4:])
+                    elif check_attribute == 'hcl':
+                        check_dict[check_attribute] = checkhcl(fields[4:])
+                    elif check_attribute == 'ecl':
+                        check_dict[check_attribute] = checkecl(fields[4:])
+                    elif check_attribute == 'pid':
+                        check_dict[check_attribute] = checkpid(fields[4:])                       
         if (check_dict['byr'] == True) and (check_dict['iyr'] == True) and (check_dict['hgt'] == True) and (check_dict['eyr'] == True) and (check_dict['hcl'] == True) and (check_dict['ecl'] == True) and (check_dict['pid'] == True):
             valid_count += 1
             valid_passports[passport_ID] = "Valid"
             print(dict_passports[passport_ID])
-    
     return valid_count
 
-if __name__ == '__main__':
-    print(str(checkPassports(load_batch_data()))+ " Valid passports")
-    print("Running Unit Test")
-    test = checkPassports(load_test_data())
-    if test == 2:
-        print("Unit Test succesful")
-        print(test)
-    else:
-        print("Unit test failed")
+if __name__ =='__main__':
+    print("Running Unit Tests")
+    print('Valid Checks ')   
+    print(checkbyr('1980'))
+    print(checkeyr('2030'))
+    print(checkhcl('#623a2f'))
+    print(checkhcl('#a97842'))
+    print(checkhcl('#888785'))
+    print(checkhgt('165cm'))
+    print(checkhgt('74in'))
+    print(checkiyr('2012')) 
+    print(checkecl('brn'))
+    print(checkpid('000000001'))
+    print('Invalid Checks')
+    print(checkbyr('2007'))
+    print(checkeyr('2038'))
+    print(checkhcl('##123abz'))
+    print(checkhcl('123abc'))
+    print(checkhgt('190'))
+    print(checkhgt('190in'))
+    print(checkiyr('2023'))
+    print(checkecl('wat'))
+    print(checkpid('0123456789'))
     
-            
-        
-
+    print(str(checkPassports(load_batch_data()))+ " Valid passports")    
