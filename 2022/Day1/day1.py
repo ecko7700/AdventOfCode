c64dcd0d36b739893255f588162358d41e6d9c43
edwardmills <ed.ed.mills@gmail.com>
2022-12-01 17:35:22 +0200

    Day 1 solution

--- 2022/Day1/day1.py
+++ 2022/Day1/day1.py
@@ -64,7 +64,42 @@ def loadData(filePath):
     
     for line in f:
         calorieList.append(line.strip())
-    
+
     return calorieList
 
-print(loadData(filePath))
+def calcCalories(calorieList):
+    resultList = []
+    result = 0
+
+    for L,calorie in enumerate(calorieList):
+        if calorie != "":
+            result += int(calorie)
+        else:
+            resultList.append(result)
+            result = 0
+    resultList.append(result)
+
+    resultList.sort(reverse=True)
+    print(resultList[0:5])
+    return sum(resultList[0:3])
+
+print(calcCalories(loadData(filePath)))
+#print(calcCalories(loadData("unitTestData.txt")))
+
+"""
+--- Part Two ---
+
+By the time you calculate the answer to the Elves' question, 
+they've already realized that the Elf carrying the most Calories of food might eventually run out of snacks.
+
+To avoid this unacceptable situation, 
+the Elves would instead like to know the total Calories carried by the top three Elves carrying the most Calories. 
+That way, even if one of those Elves runs out of snacks, they still have two backups.
+
+In the example above, the top three Elves are the fourth Elf (with 24000 Calories), 
+then the third Elf (with 11000 Calories), then the fifth Elf (with 10000 Calories). 
+The sum of the Calories carried by these three elves is 45000.
+
+Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
+
+"""
